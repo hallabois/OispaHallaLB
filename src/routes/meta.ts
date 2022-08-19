04d8919c3eddcf49d9154707a344bc22f829f419
Vizitys <53524661+Vizitys@users.noreply.github.com>
2022-08-19 13:55:50 +0300

    updated the meta route to use tokens

--- src/routes/meta.ts
+++ src/routes/meta.ts
@@ -1,30 +1,29 @@
 import express from "express";
 import { Types } from "mongoose";
 import { validateScreenName } from "../models/user";
+import { validate_token } from "../io/oispahalla";
 
 import { User } from "./scores";
 
 const router = express.Router();
-
-router.get("/verifyid/:id", async (req, res, next) => {
-  const isValid = Types.ObjectId.isValid(req.params.id);
-  res.status(200).json({ valid: isValid });
-});
 
 router.get("/verifyname/:name", async (req, res, next) => {
   const isValid = validateScreenName(req.params.name);
   res.status(200).json({ valid: isValid.valid, error: isValid.error });
 });
 
-router.post("/changename/:id", async (req, res, next) => {
-  if (!Types.ObjectId.isValid(req.params.id)) {
-    return res.status(400).json({ message: "Invalid id" });
+router.post("/changename/:token", async (req, res, next) => {
+  let tokenRes = await validate_token(req.params.token);
+  if (!tokenRes.valid || !tokenRes.user_data) {
+    return res.status(401).json({ message: "Invalid token" });
   }
+
   if (!req.body.name) {
     return res.status(400).json({ message: "Missing name" });
   }
-  User.findByIdAndUpdate(
-    req.params.id,
+
+  User.findOneAndUpdate(
+    { uid: tokenRes.user_data.uid },
     { screenName: req.body.name },
     { runValidators: true, context: "query" },
     (err, user) => {
