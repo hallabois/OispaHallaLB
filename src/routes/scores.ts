43f65b00d20aad2a2cf0d2cf822efe02b95e3191
Vizitys <53524661+Vizitys@users.noreply.github.com>
2022-09-01 23:47:52 +0300

    fetchboard returns the size it's supposed to

--- src/routes/scores.ts
+++ src/routes/scores.ts
@@ -253,7 +253,7 @@ export async function getByTokenAndRank(req, res) {
                     }
                   }
 
-                  topBoard = topBoard.slice(0, +req.params.maxnum - 1);
+                  topBoard = topBoard.slice(0, +req.params.maxnum);
                   score.user = user;
                   res.status(200).json({
                     topBoard,
